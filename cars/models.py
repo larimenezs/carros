661197243b3c42e45d5b901b9ff38109e624723b
Loyane <larissasezenem@gmail.com>
2024-08-15 00:30:30 -0300

    update admin

--- cars/models.py
+++ cars/models.py
@@ -4,6 +4,9 @@ class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
