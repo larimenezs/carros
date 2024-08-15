7b367e36d4b96a07ae00ef615fd4b3358e8d5407
Loyane <larissasezenem@gmail.com>
2024-08-14 22:55:41 -0300

    created new model

--- cars/models.py
+++ cars/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
