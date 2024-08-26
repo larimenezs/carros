26848f6e2a65848331cfc9d6edc991ed4dcb70b3
Loyane <larissasezenem@gmail.com>
2024-08-26 17:55:15 -0300

    upload image car

--- cars/models.py
+++ cars/models.py
@@ -13,7 +13,9 @@ class Car(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
 
     def __str__(self):
