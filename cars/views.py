3cb056b1f50e05e7498fe2222ef83ce70c02a53c
Loyane <larissasezenem@gmail.com>
2024-09-02 19:06:22 -0300

    update render

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from cars.models import Car
 
 def cars_view(request):
+    cars = Car.objects.all() 
+
     return render(
         request, 
         'cars.html',
-        {'cars': {'model': 'Astra 2.0'}}
+        {'cars': cars}
     )
