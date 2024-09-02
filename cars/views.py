903a998632617b2f10c290a6dc6e0defacf19e67
Loyane <larissasezenem@gmail.com>
2024-09-02 19:37:49 -0300

    update views

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all() 
+    cars = Car.objects.filter(model__contains='Chevette Tubarão') 
 
     return render(
         request, 
