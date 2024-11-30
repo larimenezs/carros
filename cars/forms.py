7eac5bfe1730940c7781d2266b44819507a2b487
Larissa Menezes <larissasezenem@gmail.com>
2024-11-30 15:06:33 -0300

    form validation

--- cars/forms.py
+++ cars/forms.py
@@ -6,3 +6,9 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo do carro deve ser de R$20.000')
+        return value
