36564181b1b5eaf00fa00b3300a6ec4191d6eeef
Larissa Menezes <larissasezenem@gmail.com>
2024-12-03 14:38:44 -0300

    new app created

--- cars/forms.py
+++ cars/forms.py
@@ -12,3 +12,9 @@ class CarModelForm(forms.ModelForm):
         if value < 20000:
             self.add_error('value', 'Valor mínimo do carro deve ser de R$20.000')
         return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Não é possível cadastrar carros antes do ano de 1975')
+        return factory_year
