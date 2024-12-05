afa96bc125c3afcf4c2f8adfe53f0e2bad1d787c
Larissa Menezes <larissasezenem@gmail.com>
2024-12-05 10:53:22 -0300

    view accounts created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
-# Create your views here.
+def register_view(request):
+    user_form = UserCreationForm()
+    return render(request, 'register.html', {'user_form': user_form})
--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ from cars.views import cars_view, new_car_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register_view, name='register'),
     path('cars/', cars_view, name='cars_list'),
     path('new_car/', new_car_view, name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
