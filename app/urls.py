3a6804132e6c2233f01cc871502a6d5aac0c0a00
Larissa Menezes <larissasezenem@gmail.com>
2024-12-05 11:36:47 -0300

    view updated

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, new_car_view
+from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
