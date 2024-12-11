4ceb00703f58cf6b3ed5eda91c880e7caff469bb
Larissa Menezes <larissasezenem@gmail.com>
2024-12-10 21:32:22 -0300

    login screen created

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect 
 
 def register_view(request):
@@ -6,7 +7,20 @@ def register_view(request):
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
-            return redirect('login')
+            return redirect('cars_list')
     else:
         user_form = UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        username = request.POST["password"]
+        user = authenticate(request, username=username, password= password)
+        if user is not None:
+            ...
+        else:
+            login_form = AuthenticationForm()
+    else:
+        login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
